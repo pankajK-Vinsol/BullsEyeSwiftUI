563228dd28e3011b10bc3ed64f86fac00931c932
Pankaj Kumar <pankaj.kumar@vinsol.com>
2020-04-16 14:22:12 +0530

    change orientation to landscape and add some items using Swift UI

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -9,13 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var alertIsVisible: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+          // Target row
+          HStack {
+            Text("Put the bullseye as close as you can to:")
+            Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
+          }
+          
+          // Slider row
+          HStack {
+            Text("1")
+            Slider(value: /*@START_MENU_TOKEN@*/.constant(10)/*@END_MENU_TOKEN@*/)
+            Text("100")
+          }
+          
+          // Button row
+          Button(action: {
+            print("Button pressed!")
+            self.alertIsVisible = true
+          }) {
+            Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+          }
+          .alert(isPresented: $alertIsVisible) { () -> Alert in
+            return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome!")))
+          }
+          
+          // Score row
+          
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: UIScreen.main.bounds.height, height: UIScreen.main.bounds.width))
     }
 }

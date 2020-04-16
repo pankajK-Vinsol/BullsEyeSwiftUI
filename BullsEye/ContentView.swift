1c7ee21a5964679738c4edfcfc7bda69090616cb
Pankaj Kumar <pankaj.kumar@vinsol.com>
2020-04-16 17:04:38 +0530

    Add spacing and padding for items, update slider for dynamic value and show it on alert

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -9,37 +9,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     
     var body: some View {
-        VStack {
-          // Target row
-          HStack {
-            Text("Put the bullseye as close as you can to:")
-            Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
-          }
-          
-          // Slider row
-          HStack {
-            Text("1")
-            Slider(value: /*@START_MENU_TOKEN@*/.constant(10)/*@END_MENU_TOKEN@*/)
-            Text("100")
-          }
-          
-          // Button row
-          Button(action: {
-            print("Button pressed!")
-            self.alertIsVisible = true
-          }) {
-            Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
-          }
-          .alert(isPresented: $alertIsVisible) { () -> Alert in
-            return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome!")))
-          }
-          
-          // Score row
-          
+      VStack {
+        Spacer()
+        
+        // Target row
+        HStack {
+          Text("Put the bullseye as close as you can to:")
+          Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
         }
+        Spacer()
+        
+        // Slider row
+        HStack {
+          Text("1")
+          Slider(value: self.$sliderValue, in: 1...100)
+          Text("100")
+        }
+        .padding(.leading, 20)
+        .padding(.trailing, 20)
+        Spacer()
+        
+        // Button row
+        Button(action: {
+          print("Button pressed!")
+          self.alertIsVisible = true
+        }) {
+          Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+        }
+        .alert(isPresented: $alertIsVisible) { () -> Alert in
+          let roundedValue: Int = Int(self.sliderValue.rounded())
+          return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue)."), dismissButton: .default(Text("Awesome!")))
+        }
+        Spacer()
+        
+        // Score row
+        HStack {
+          Button(action: {}) {
+            Text("Start Over")
+          }
+          Spacer()
+          Text("Score:")
+          Text("999999")
+          Spacer()
+          Text("Round:")
+          Text("999")
+          Spacer()
+          Button(action: {}) {
+            Text("Info")
+          }
+        }
+        .padding(.bottom, 20)
+        .padding(.leading, 20)
+        .padding(.trailing, 20)
+      }
     }
 }
 
